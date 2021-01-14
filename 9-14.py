c7056f9f618e2b6df2162d94f6ab35358b982ac9
Márton Urbán <urban.marton@gmail.com>
2021-01-14 22:13:04 +0100

    9-14: Changed to for loop, but still produces dubplicates sometimes.

--- 9-14.py
+++ 9-14.py
@@ -1,11 +1,8 @@
-from random import choice, randint
+from random import randint
 
 my_list = [2, 33, 44, 56, 7, 5, 34, 56, 54, 7, 'b', 'r', 't', 'a', 'e']
 
-print(
-    f"The winning combination:"
-    f"{my_list.pop(randint(0, 14))},"
-    f"{my_list.pop(randint(0, 13))},"
-    f"{my_list.pop(randint(0, 12))},"
-    f"{my_list.pop(randint(0, 11))}"
-)
+print("The winning combination:")
+for char in range(4):
+    length_of_list = len(my_list) - 1
+    print(my_list.pop(randint(0, length_of_list)))
