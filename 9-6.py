969675350dbfb0947e2c92b534f59fcbfdb2f466
Márton Urbán <urban.marton@gmail.com>
2021-01-14 15:04:16 +0100

    9-6: Finished exercise

--- 9-6.py
+++ 9-6.py
@@ -25,4 +25,19 @@ class Restaurant():
         self.number_served += incrementby
 
 
+class IceCreamStand(Restaurant):
+    def __init__(self, name, cuisine_type='fagyizó'):
+        super().__init__(name, cuisine_type)
+
+    def display_flavors(self):
+        print("\nThe following flavors are available:")
+        for flavor in self.flavors:
+            print(f"\t- {flavor}")
+
+
 restaurant = Restaurant('the mean queen', 'pizza')
+
+icecream_place = IceCreamStand('sarki fagyizó')
+icecream_place.flavors = ['chocolate', 'lemon', 'strawbery', 'nutella']
+
+icecream_place.display_flavors()
