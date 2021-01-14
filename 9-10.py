a630864b0b632cfb961ee25a4e0e1c2b4845494c
Márton Urbán <urban.marton@gmail.com>
2021-01-14 21:16:07 +0100

    9-10: Finished Exercise

---
+++ 9-10.py
@@ -0,0 +1,5 @@
+from modules.restaurants import Restaurant
+
+channel_club = Restaurant('the channel club', 'steak and seafood')
+channel_club.describe_restaurant()
+channel_club.open_restaurant()
---
+++ modules/restaurants.py
@@ -0,0 +1,25 @@
+class Restaurant():
+    """A class representing a restaurant."""
+
+    def __init__(self, name, cuisine_type):
+        """Initialize the restaurant."""
+        self.name = name.title()
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """Display a summary of the restaurant."""
+        msg = f"{self.name} serves wonderful {self.cuisine_type}."
+        print(f"\n{msg}")
+
+    def open_restaurant(self):
+        """Display a message that the restaurant is open."""
+        msg = f"{self.name} is open. Come on in!"
+        print(f"\n{msg}")
+
+    def set_number_served(self, number_served):
+        """Updates the number of served customers of given restaurant"""
+        self.number_served = number_served
+
+    def increment_number_served(self, incrementby):
+        self.number_served += incrementby
