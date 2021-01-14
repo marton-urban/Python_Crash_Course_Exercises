ac7018914c164cf4a9af52bc9b0f43330ef61f18
Márton Urbán <urban.marton@gmail.com>
2021-01-14 22:02:15 +0100

    9-14: Changed tuple to list, choice to randint (with pop)

--- 9-14.py
+++ 9-14.py
@@ -1,5 +1,11 @@
-from random import choice
+from random import choice, randint
 
-my_tuple = (2, 33, 44, 56, 7, 5, 34, 56, 54, 7, 'b', 'r', 't', 'a', 'e')
+my_list = [2, 33, 44, 56, 7, 5, 34, 56, 54, 7, 'b', 'r', 't', 'a', 'e']
 
-print(f"The winning combination: {choice(my_tuple)}, {choice(my_tuple)}, {choice(my_tuple)}, {choice(my_tuple)}")
+print(
+    f"The winning combination:"
+    f"{my_list.pop(randint(0, 14))},"
+    f"{my_list.pop(randint(0, 13))},"
+    f"{my_list.pop(randint(0, 12))},"
+    f"{my_list.pop(randint(0, 11))}"
+)
