b2daf379652ffff64066aae020c1199f173fd6c8
Márton Urbán <urban.marton@gmail.com>
2021-01-14 21:53:24 +0100

    9-14: Initial commit

---
+++ 9-14.py
@@ -0,0 +1,5 @@
+from random import choice
+
+my_tuple = (2, 33, 44, 56, 7, 5, 34, 56, 54, 7, 'b', 'r', 't', 'a', 'e')
+
+print(f"The winning combination: {choice(my_tuple)}, {choice(my_tuple)}, {choice(my_tuple)}, {choice(my_tuple)}")
