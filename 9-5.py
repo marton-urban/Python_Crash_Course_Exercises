9732992b0bb79e49f75b888aed6d82aefdd86b84
Márton Urbán <urban.marton@gmail.com>
2021-01-14 12:47:15 +0100

    9-5: Finished exercise

--- 9-5.py
+++ 9-5.py
@@ -7,6 +7,7 @@ class User:
         self.age = age
         self.first_name = first_name.title()
         self.last_name = last_name.title()
+        self.login_attempts = 0
 
     def describe_user(self):
         print(
@@ -20,7 +21,20 @@ class User:
     def greet_user(self):
         print(f"\nWelcome to my program, dear {self.first_name}!")
 
+    def increment_login_attempts(self, incrementby):
+        self.login_attempts += incrementby
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 first_user = User('márton', 'urbán', 33, 'hungary', 'programming')
-User.describe_user(first_user)
-User.greet_user(first_user)
+first_user.increment_login_attempts(2)
+first_user.increment_login_attempts(3)
+first_user.increment_login_attempts(4)
+print(first_user.login_attempts)
+
+first_user.reset_login_attempts()
+print(first_user.login_attempts)
+
+
