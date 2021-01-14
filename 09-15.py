cb3daaf2ffdb1b86ad9ed0db11fad5b146296c38
urban.marton <urban.marton@gmail.com>
2021-01-15 00:48:46 +0100

    9-15: Order does not matter + player around a bit

--- 09-15.py
+++ 09-15.py
@@ -1,22 +1,28 @@
 from random import randint
 
-my_ticket = [33, 't', 7, 'a']
+my_ticket = (33, 't', 7, 'a')
 counter = 0
 highest = 0
 
-while counter < 200000:
+while counter < 260000:
     if counter > highest:
         highest = counter
         print(highest)
-    winning_ticket = []
     counter = 0
-    while winning_ticket != my_ticket:
+    right_counter = 0
+    while right_counter != 4:
         my_list = [2, 33, 44, 7, 9, 5, 34, 56, 54, 243, 'b', 'r', 't', 'a', 'e']
         winning_ticket = []
         counter += 1
+        right_counter = 0
         for char in range(4):
             length_of_list = len(my_list) - 1
-            winning_ticket.append(my_list.pop(randint(0, length_of_list)))
+            current = my_list.pop(randint(0, length_of_list))
+            if current not in my_ticket:
+                break
+            else:
+                winning_ticket.append(current)
+                right_counter += 1
 
 print("Your ticket: ", end="")
 print(*my_ticket)
