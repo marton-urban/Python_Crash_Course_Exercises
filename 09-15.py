da0f7ef73c7fe411b7937eda2b41e1bb5c938804
Márton Urbán <urban.marton@gmail.com>
2021-01-15 10:22:19 +0100

    9-15: Used classes + order does not matter

--- 09-15.py
+++ 09-15.py
@@ -1,27 +1,44 @@
 from random import choice
 
 
+def check_if_won():
+    for element in my_ticket.randomized:
+        if element not in winning_ticket.randomized:
+            my_ticket.won = False
+            break
+        else:
+            my_ticket.won = True
+
+
 class RandomSelect4:
     """Pulls 4 cards from possibilities + checks my_ticket against winning_ticket"""
 
-    def randomize(self):
-        self.randomize = []
-        while len(self.randomize) < 4:
-            pulled_item = choice(possibilities)
-            if pulled_item not in self.randomize:
-                self.randomize.append(pulled_item)
+    def __init__(self, won=False):
+        self.randomized = []
+        self.won = won
 
-    def isnota(self, winning_ticket):
-        self.winning_ticket = winning_ticket
+    def randomize(self):
+        self.randomized = []
+        while len(self.randomized) < 4:
+            pulled_item = choice(possibilities)
+            if pulled_item not in self.randomized:
+                self.randomized.append(pulled_item)
+        if self == my_ticket:
+            check_if_won()
 
 
 possibilities = [2, 33, 44, 7, 9, 5, 34, 56, 54, 243, 'b', 'r', 't', 'a', 'e']
 my_ticket = RandomSelect4()
 winning_ticket = RandomSelect4()
-
 plays = 0
-won = False
-while not won:
-    my_ticket.randomize
-    winning_ticket.randomize
+
+while not my_ticket.won:
+    winning_ticket.randomize()
+    my_ticket.randomize()
     plays += 1
+
+print("Your ticket: ", end='')
+print(*my_ticket.randomized)
+print("Winning ticket: ", end='')
+print(*winning_ticket.randomized)
+print(f"It only took {plays} tries to win!")
