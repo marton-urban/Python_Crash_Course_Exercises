b7d7f971bc395a4610969f0472d862a36cf1160b
Márton Urbán <urban.marton@gmail.com>
2021-01-16 12:33:27 +0100

    11-02: Second test, not working (as described in the exercise)
    p.s.: had to rename 11-02 to eleven_per_two to be able to test in 11-02_test.py

--- eleven_per_two.py
+++ eleven_per_two.py
@@ -1,2 +1,2 @@
-def city_country(city_name, country_name):
-    return f"{city_name.title()}, {country_name.title()}"
+def city_country(city_name, country_name, population):
+    return f"{city_name.title()}, {country_name.title()} - population {population}"
