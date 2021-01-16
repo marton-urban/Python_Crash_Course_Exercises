7ec22c9be546573cca6685ed1919739a3ae441d6
Márton Urbán <urban.marton@gmail.com>
2021-01-16 12:41:05 +0100

    11-02: Added test_city_country_population test
    p.s.: had to rename 11-02 to eleven_per_two to be able to test in 11-02_test.py

--- 11-02_test.py
+++ 11-02_test.py
@@ -7,6 +7,9 @@ class CityCountryTestCase(unittest.TestCase):
     def test_city_country(self):
         self.assertEqual(city_country('santiago', 'chile'), 'Santiago, Chile')
 
+    def test_city_country_population(self):
+        self.assertEqual(city_country('santiago', 'chile', 50000), 'Santiago, Chile - population 50000')
+
 
 if __name__ == '__main__':
     unittest.main()
