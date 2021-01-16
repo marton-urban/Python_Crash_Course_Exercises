19b6a4e1ebaf2e2831b573df67c2b14f964f9761
Márton Urbán <urban.marton@gmail.com>
2021-01-16 12:25:51 +0100

    11-01: had to rename 11-01 to eleven_per_one to be able to test in 11-01_test.py

---
+++ 11-01_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from eleven_per_one import city_country
+
+class CityCountryTestCase (unittest.TestCase):
+    def test_city_country(self):
+        self.assertEqual(city_country('santiago', 'chile'), 'Santiago, Chile')
+
+if __name__ == '__main__':
+    unittest.main()
